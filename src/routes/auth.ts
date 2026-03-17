0eb1f12151f8ef5d098359c34d5ae8bcf71c3945
rachynska-anastasiia <rachynska.anastasiia.work@gmail.com>
2026-03-17 17:35:20 +0200

    видалення файлів, що не використовуються

--- src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { register } from "node:module";
-const authRoutes = Router();
-
-authRoutes.post("/register", () => {});
-authRoutes.post("/login", () => {});
-authRoutes.get("/profile", () => {});
-
-export default authRoutes;
